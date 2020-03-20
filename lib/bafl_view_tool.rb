532d79f3d6f8a5aa18c236e434a968e9ee9f2ad4
Uglybf <Uglybf@users.noreply.github.com>
2020-03-20 20:25:47 +0100

    Dev tool initial commit

---
+++ lib/bafl_view_tool.rb
@@ -0,0 +1,6 @@
+require "bafl_view_tool/version"
+
+module BaflViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bafl_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BaflViewTool
+  VERSION = "0.1.0"
+end
